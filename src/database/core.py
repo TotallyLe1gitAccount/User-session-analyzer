1ccbccafc36851b227610207a7dd0f31d808a8bf
TotallyLe1gitAccount <tllylgitacnut@gmail.com>
2026-04-09 08:03:40 +0300

    feat(database): added protected _create database method to create table sessions

--- src/database/core.py
+++ src/database/core.py
@@ -4,6 +4,17 @@ class Database:
     def __init__(self, path):
         self.conn = sqlite3.connect(path)
         self.cur = self.conn.cursor()
+        self._create_database()
+
+    def _create_database(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS sessions(
+                         session_id INTEGER PRIMARY KEY AUTOINCREMENT
+                         activity TEXT NOT NULL
+                         duration_minutes INT NOT NULL
+                         session_date TEXT NOT NULL
+                         notes TEXT
+                         )""")
+        self.conn.commit()
 
 
 

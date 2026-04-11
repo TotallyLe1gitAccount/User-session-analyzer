c7afd73b52a0a37656a6e698b8ce1282c7e50f15
TotallyLe1gitAccount <tllylgitacnut@gmail.com>
2026-04-11 08:26:14 +0300

    feat(test): added test for edit_session method in TestDB class

--- tests/test_database.py
+++ tests/test_database.py
@@ -38,7 +38,26 @@ class TestDB(unittest.TestCase):
         result = db.cur.fetchone()
 
         self.assertIsNone(result, "Пустые данные")
-        
+
+    def test_edit_session(self):
+        db = Database(":memory:")
+
+        db.add_session("Gym", 60, "04-10-2026", "Leg day")
+        db.edit_session(1, activity="Read a book", 
+                        duration=60, 
+                        date="04-10-2026 12:00", 
+                        notes="Atomic habits")
+
+        db.cur.execute("""SELECT * FROM sessions WHERE session_id = 1""")
+        result = db.cur.fetchone()
+
+        assert result == (
+        1,
+        "Read a book",
+        60,
+        "04-10-2026 12:00",
+        "Atomic habits"
+        )
 
 if __name__ == "__main__":
     unittest.main(exit=False)

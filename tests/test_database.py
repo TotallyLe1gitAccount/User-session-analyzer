f297d5f06bf3261fb3d5b586a1078b4bf956fe3b
TotallyLe1gitAccount <tllylgitacnut@gmail.com>
2026-04-21 08:27:11 +0300

    feat(tests): refactored class TestDB

--- tests/test_database.py
+++ tests/test_database.py
@@ -6,75 +6,86 @@ from database.core import Database
 
 
 class TestDB(unittest.TestCase):
+
+    def setUp(self):
+        self.db = Database(":memory:")
         
     def test_sessions_table_created(self):
-        db = Database(":memory:")
 
-        db.cur.execute("""
+        self.db.cur.execute("""
                        SELECT name
                        FROM sqlite_master
                        WHERE type='table' AND name='sessions' """)
-        result = db.cur.fetchone()
+        result = self.db.cur.fetchone()
 
         self.assertIsNotNone(result, "Таблица не создана.")
         
     def test_add_session(self):
-        db = Database(":memory:")
 
-        db.add_session("Gym", 60, "04-10-2026", "Leg day")
+        self.db.add_session("Gym", 60, "04-10-2026", "Leg day")
 
-        db.cur.execute("""SELECT * FROM sessions""")
-        result = db.cur.fetchone()
+        self.db.cur.execute("""SELECT * FROM sessions""")
+        result = self.db.cur.fetchone()
 
-        self.assertIsNotNone(result, "Пустые данные")
+        self.assertEqual(result[1], "Gym")
+        self.assertEqual(result[2], 60)
+        self.assertEqual(result[3], "04-10-2026")
+        self.assertEqual(result[4], "Leg day")
 
     def test_delete_session(self):
-        db = Database(":memory:")
         
-        db.add_session("Gym", 60, "04-10-2026", "Leg day")
-        db.delete_session(1)
+        self.db.add_session("Gym", 60, "04-10-2026", "Leg day")
 
-        db.cur.execute("""SELECT * FROM sessions""")
-        result = db.cur.fetchone()
+        self.db.cur.execute("SELECT session_id FROM sessions")
+        session_id = self.db.cur.fetchone()[0]
+
+        self.db.delete_session(session_id)
+
+        self.db.cur.execute("""SELECT * FROM sessions""")
+        result = self.db.cur.fetchone()
 
         self.assertIsNone(result, "Пустые данные")
 
     def test_delete_nonexistent(self):
-        
-        db = Database(":memory:")
-        db.delete_session(999)
-        db.cur.execute("SELECT COUNT(*) FROM sessions")
-        count = db.cur.fetchone()[0]
+        self.db.delete_session(999)
+        self.db.cur.execute("SELECT COUNT(*) FROM sessions")
+        count = self.db.cur.fetchone()[0]
         self.assertEqual(count, 0)
+
+    def test_edit_full_session(self):
+   
+        self.db.add_session("Gym", 60, "04-10-2026", "Leg day")
+
+        
+        self.db.cur.execute("SELECT session_id FROM sessions")
+        session_id = self.db.cur.fetchone()[0]
+
+        self.db.edit_session(session_id, activity="Read a book", 
                         duration=60, 
                         date="04-10-2026 12:00", 
                         notes="Atomic habits")
 
-        db.cur.execute("""SELECT * FROM sessions WHERE session_id = 1""")
-        result = db.cur.fetchone()
 
-        assert result == (
-        1,
-        "Read a book",
-        60,
-        "04-10-2026 12:00",
-        "Atomic habits"
-        )
+        self.db.cur.execute("""SELECT * FROM sessions WHERE session_id = (?)""", (session_id,))
+        result = self.db.cur.fetchone()
+
+        self.assertEqual(result[0], session_id)
+        self.assertEqual(result[1], "Read a book")
+        self.assertEqual(result[2], 60)
+        self.assertEqual(result[3], "04-10-2026 12:00")
+        self.assertEqual(result[4], "Atomic habits")
+
     def test_read_session(self):
 
-        db = Database(":memory:")
+        self.db.add_session("Gym", 60, "04-10-2026", "Leg day")
 
-        db.add_session("Gym", 60, "04-10-2026", "Leg day")
+        result = self.db.read_session()
 
-        result = db.read_session()
-        
-        assert result == [{
-        "session_id": 1,
-        "activity": "Gym",
-        "duration_minutes": 60,
-        "session_date": "04-10-2026",
-        "notes": "Leg day"
-        }]
+        self.assertEqual(result[0]["activity"], "Gym")
+        self.assertEqual(result[0]["duration_minutes"], 60)
+        self.assertEqual(result[0]["session_date"], "04-10-2026")
+        self.assertEqual(result[0]["notes"], "Leg day")
+
 
 if __name__ == "__main__":
     unittest.main(exit=False)
